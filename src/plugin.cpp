830c8dad1f10b8dde425bc906d9bfc29358b6330
MaybeAsrak <kmanoocheri@gmail.com>
2024-03-30 16:37:31 -0400

    Initial Release

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -3,117 +3,147 @@
 #include "include\PerkEntryPointExtenderAPI.h"
 
 struct Hooks {
-    struct SetMagicTimeSlowdownStart {
-        static void thunk(RE::VATS* a_vats, float a_worldMagnitude, float a_playerMagnitude) {
-            const auto magicTimeSlowdown = a_vats->magicTimeSlowdown;
-            const auto playermagicTimeSlowdown = a_vats->playerMagicTimeSlowdown;
+        struct ShoutRecoveryHook {
+        static void thunk(RE::AIProcess* a_AP, float RecoveryTime) {
+
+            auto a_actor = a_AP->GetUserData();            
+            float f_mult = a_actor->AsActorValueOwner()->GetActorValue(RE::ActorValue::kShoutRecoveryMult);  
+            float basetime = 1.0f;
+            float shoutrecoveryfactor = 100.0f;
+            float shoutrecoverycap = 20.0f;
+            float shoutrecoverymultiplier = 100.0f;
             
-            if (a_worldMagnitude < 0) {
-                if (playermagicTimeSlowdown == 0) {
-                    func(a_vats, magicTimeSlowdown, a_worldMagnitude * -1);
-                } else {
-                    func(a_vats, magicTimeSlowdown, a_worldMagnitude * -1 * playermagicTimeSlowdown);
-                
+            if (a_AP->high->currentShoutVariation > 3) {
+                basetime = a_AP->high->currentShout->variations[0].recoveryTime;
+                RE::HandleEntryPoint(RE::PerkEntryPoint::kModSpellCost, a_actor, &shoutrecoveryfactor, "ShoutRecovery",
+                                     2, {a_AP->high->currentShout->variations[0].spell});
+                RE::HandleEntryPoint(RE::PerkEntryPoint::kModSpellCost, a_actor, &shoutrecoverycap, "ShoutRecovery",
+                                     3, {a_AP->high->currentShout->variations[0].spell});
+                RE::HandleEntryPoint(RE::PerkEntryPoint::kModSpellCost, a_actor, &shoutrecoverymultiplier,
+                                     "ShoutRecovery",
+                                     4, {a_AP->high->currentShout->variations[0].spell});
+            } else {
+                basetime = a_AP->high->currentShout->variations[a_AP->high->currentShoutVariation].recoveryTime;
+                RE::HandleEntryPoint(RE::PerkEntryPoint::kModSpellCost, a_actor, &shoutrecoveryfactor, "ShoutRecovery",
+                                     2,
+                                     {a_AP->high->currentShout->variations[a_AP->high->currentShoutVariation].spell});
+                RE::HandleEntryPoint(RE::PerkEntryPoint::kModSpellCost, a_actor, &shoutrecoverycap, "ShoutRecovery",
+                                     3,
+                                     {a_AP->high->currentShout->variations[a_AP->high->currentShoutVariation].spell});
+                RE::HandleEntryPoint(RE::PerkEntryPoint::kModSpellCost, a_actor, &shoutrecoverymultiplier,
+                                     "ShoutRecovery",
+                                     4,
+                                     {a_AP->high->currentShout->variations[a_AP->high->currentShoutVariation].spell});
+            }
+            float cappedshoutrecovery = (std::max(shoutrecoverycap, (f_mult*100.0f+shoutrecoveryfactor-100.0f)))/100.0f;
+            RecoveryTime = basetime * shoutrecoverymultiplier * cappedshoutrecovery / 100.0f;
+
+            func(a_AP, RecoveryTime);
+
+        }
+        static inline REL::Relocation<decltype(thunk)> func;
+        };
+
+    struct ShoutRecoveryMenuHook {
+        static bool thunk(RE::GFxValue::ObjectInterface* a_start, void* a_data, const char* a_name,
+                          const RE::GFxValue& a_value, bool a_isDObj) {
+            auto a_actor = RE::PlayerCharacter::GetSingleton();
+            float f_mult = a_actor->AsActorValueOwner()->GetActorValue(RE::ActorValue::kShoutRecoveryMult);
+
+            RE::AIProcess* a_AP = a_actor->GetActorRuntimeData().currentProcess;
+            RE::GFxValue selectedIndex, categoryList, inventoryLists;
+            uint32_t menu_form = 0;
+            auto* a_ui = RE::UI::GetSingleton();
+            if (a_ui->IsMenuOpen(RE::MagicMenu::MENU_NAME)) {
+                RE::ConsoleLog::GetSingleton()->Print("beep1");
+                auto* magic_menu = static_cast<RE::MagicMenu*>(a_ui->GetMenu(RE::MagicMenu::MENU_NAME).get());
+                if (magic_menu) {
+                    RE::ConsoleLog::GetSingleton()->Print("beep2");
+                    RE::GFxValue result;
+                    magic_menu->uiMovie->GetVariable(&result,
+                                                     "_root.Menu_mc.inventoryLists.itemList.selectedEntry.formId");
+                    if (result.GetType() == RE::GFxValue::ValueType::kNumber) {
+                        RE::ConsoleLog::GetSingleton()->Print("beep3");
+                        menu_form = static_cast<std::uint32_t>(result.GetNumber());
+                    }
                 }
-                   
             }
-            else {
-                if (magicTimeSlowdown == 0) {
-                    func(a_vats, a_worldMagnitude, 1.0f);
+            if (menu_form) {
+                auto a_Shout = RE::TESForm::LookupByID(menu_form)->As<RE::TESShout>();
+                if (a_Shout) {
+                    float basetime = 1.0f;
+                    float shoutrecoveryfactor = 0.0f;
+                    float shoutrecoverycap = 0.0f;
+                    float shoutrecoverymultiplier = 100.0f;
+
+                    RE::HandleEntryPoint(RE::PerkEntryPoint::kModSpellCost, a_actor, &shoutrecoveryfactor,
+                                         "ShoutRecovery", 2, {a_Shout->variations[0].spell});
+                    RE::HandleEntryPoint(RE::PerkEntryPoint::kModSpellCost, a_actor, &shoutrecoverycap, "ShoutRecovery",
+                                         3, {a_Shout->variations[0].spell});
+                    RE::HandleEntryPoint(RE::PerkEntryPoint::kModSpellCost, a_actor, &shoutrecoverymultiplier,
+                                         "ShoutRecovery", 4, {a_Shout->variations[0].spell});
+
+                    basetime = a_Shout->variations[0].recoveryTime;
+                    float cappedshoutrecovery =
+                        (std::max(shoutrecoverycap, (f_mult * 100.0f + shoutrecoveryfactor))) / 100.0f;
+                    float RecoveryTime1 = basetime * shoutrecoverymultiplier * cappedshoutrecovery / 100.0f;
+
+                    shoutrecoveryfactor = 0.0f;
+                    shoutrecoverycap = 0.0f;
+                    shoutrecoverymultiplier = 100.0f;
+
+                    RE::HandleEntryPoint(RE::PerkEntryPoint::kModSpellCost, a_actor, &shoutrecoveryfactor,
+                                         "ShoutRecovery", 2, {a_Shout->variations[1].spell});
+                    RE::HandleEntryPoint(RE::PerkEntryPoint::kModSpellCost, a_actor, &shoutrecoverycap, "ShoutRecovery",
+                                         3, {a_Shout->variations[1].spell});
+
+                    RE::HandleEntryPoint(RE::PerkEntryPoint::kModSpellCost, a_actor, &shoutrecoverymultiplier,
+                                         "ShoutRecovery", 4, {a_Shout->variations[1].spell});
+                    basetime = a_Shout->variations[1].recoveryTime;
+                    cappedshoutrecovery =
+                        (std::max(shoutrecoverycap, (f_mult * 100.0f + shoutrecoveryfactor))) / 100.0f;
+                    float RecoveryTime2 = basetime * shoutrecoverymultiplier * cappedshoutrecovery / 100.0f;
+
+                    shoutrecoveryfactor = 0.0f;
+                    shoutrecoverycap = 0.0f;
+                    shoutrecoverymultiplier = 100.0f;
+
+                    RE::HandleEntryPoint(RE::PerkEntryPoint::kModSpellCost, a_actor, &shoutrecoveryfactor,
+                                         "ShoutRecovery", 2, {a_Shout->variations[2].spell});
+                    RE::HandleEntryPoint(RE::PerkEntryPoint::kModSpellCost, a_actor, &shoutrecoverycap, "ShoutRecovery",
+                                         3, {a_Shout->variations[2].spell});
+
+                    RE::HandleEntryPoint(RE::PerkEntryPoint::kModSpellCost, a_actor, &shoutrecoverymultiplier,
+                                         "ShoutRecovery", 4, {a_Shout->variations[2].spell});
+                    basetime = a_Shout->variations[2].recoveryTime;
+                    cappedshoutrecovery =
+                        (std::max(shoutrecoverycap, (f_mult * 100.0f + shoutrecoveryfactor))) / 100.0f;
+                    float RecoveryTime3 = basetime * shoutrecoverymultiplier * cappedshoutrecovery / 100.0f;
+
+                    char buffer[80];
+                    sprintf_s(buffer, "%.0f, %.0f, %.0f", RecoveryTime1, RecoveryTime2, RecoveryTime3);
+                    auto a_updatedvalue = const_cast<RE::GFxValue&>(a_value);
+                    a_updatedvalue.SetString(buffer);
+
+                    return func(a_start, a_data, a_name, a_updatedvalue, a_isDObj);
+
                 } else {
-                    func(a_vats, magicTimeSlowdown * a_worldMagnitude, playermagicTimeSlowdown);
+                    return func(a_start, a_data, a_name, a_value, a_isDObj);
                 }
-                   
             }
+            return func(a_start, a_data, a_name, a_value, a_isDObj);
         }
         static inline REL::Relocation<decltype(thunk)> func;
     };
 
-        struct GetActiveEffectMagnitude {
-        static float thunk(RE::ActiveEffect* a_AE) {
-            float timescale;
-            const auto magicTime = RE::VATS::GetSingleton();
-            float playertimescale = magicTime->playerMagicTimeSlowdown;
-            if (playertimescale == 0.0f) {
-                playertimescale = 1.0f;
-            }
-           if (a_AE->effect->baseEffect->IsHostile()) {
-           
-           timescale = a_AE->GetMagnitude()*-1;          
-           a_AE->duration = a_AE->duration * timescale * -1 * playertimescale;
-           }
-           else {
-           timescale = a_AE->GetMagnitude();
-           a_AE->duration = a_AE->duration * playertimescale;
-           }
-           return timescale;
-        }
-        static inline REL::Relocation<decltype(thunk)> func;
-    };
 
-    struct SlowTimeStop {
-        static void thunk(RE::SlowTimeEffect* a_SlowTime) {
-           const auto magicTime = RE::VATS::GetSingleton();
-           auto magicTimeSlowdown = magicTime->magicTimeSlowdown;
-           auto playermagicTimeSlowdown = magicTime->playerMagicTimeSlowdown;
-           float timescale;
-           if (a_SlowTime->effect->baseEffect->IsHostile()) {
-           timescale = a_SlowTime->GetMagnitude();
-           magicTime->SetMagicTimeSlowdown((magicTimeSlowdown), playermagicTimeSlowdown/timescale);
-           } else {
-           timescale = a_SlowTime->GetMagnitude();
-           magicTime->SetMagicTimeSlowdown((magicTimeSlowdown / timescale), playermagicTimeSlowdown);
-           }
-           func(a_SlowTime);
-        }
-        static inline REL::Relocation<decltype(thunk)> func;
-    };
-
-    struct NullMagicTimeSlowdownStop {
-        static void thunk(RE::VATS* a_vats, float a_worldMagnitude, float a_playerMagnitude) {
-        }
-        static inline REL::Relocation<decltype(thunk)> func;
-    };
-
-    struct NullGetCurrentMultiplier {
-        static float thunk(RE::VATS* a_vats) {
-            return 1.0f;
-        }
-        static inline REL::Relocation<decltype(thunk)> func;
-    };
-
-    // struct PlayerCharacter_Update {
-    //     static void thunk(RE::PlayerCharacter* a_player, float a_delta) {
-    //        func(a_player, a_delta);
-    //    }
-    //    static inline REL::Relocation<decltype(thunk)> func;
-    //};
 
     static void Install() {
-        // stl::write_vfunc<RE::PlayerCharacter, 0xAD, PlayerCharacter_Update>();
+        REL::Relocation<std::uintptr_t> functionA{RELOCATION_ID(41737, 42818), REL::Relocate(0xC3, 0xC3)};
+        stl::write_thunk_call<ShoutRecoveryHook>(functionA.address());
 
-        REL::Relocation<std::uintptr_t> checkslowtime{RELOCATION_ID(529316, 34972), REL::Relocate(0x0, 0x0)};
-        REL::safe_fill(checkslowtime.address(), REL::NOP, 0x13);
-
-        stl::write_vfunc<RE::SlowTimeEffect, 0x13, SlowTimeStop>();
-
-
-        REL::Relocation<std::uintptr_t> FunctionNullGetCurrentMultiplier{RELOCATION_ID(34175, 34968),
-                                                                          REL::Relocate(0x6E, 0x6E)};
-        stl::write_thunk_call<NullGetCurrentMultiplier>(FunctionNullGetCurrentMultiplier.address());
-        
-
-        REL::Relocation<std::uintptr_t> functionNullMagicTimeSlowdownStop{RELOCATION_ID(34177, 34970),
-                                                                          REL::Relocate(0x35, 0x35)};
-        stl::write_thunk_call<NullMagicTimeSlowdownStop>(functionNullMagicTimeSlowdownStop.address());
-
-
-         REL::Relocation<std::uintptr_t> functionSetMagicTimeSlowdownStart{RELOCATION_ID(34175, 34968), REL::Relocate(0x5D, 0x5D)};
-         stl::write_thunk_call<SetMagicTimeSlowdownStart>(functionSetMagicTimeSlowdownStart.address());
-
-         REL::Relocation<std::uintptr_t> functionGetActiveEffectMagnitude{RELOCATION_ID(34175, 34968), REL::Relocate(0x4C, 0x4C)};
-         stl::write_thunk_call<GetActiveEffectMagnitude>(functionGetActiveEffectMagnitude.address());
-
+        REL::Relocation<std::uintptr_t> functionB{RELOCATION_ID(51020, 51898), REL::Relocate(0x459, 0x45B)};
+        stl::write_thunk_call<ShoutRecoveryMenuHook>(functionB.address());
 
     }
 };
